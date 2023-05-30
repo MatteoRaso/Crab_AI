2ef52a5185dbd24e54786319a586ee5f09b2341e
Matteo Raso <matteo_luigi_raso@protonmail.com>
2023-05-30 00:44:20 -0400

    First attempt
    Results are a bit poor, but this is just meant to show that an NN approach
    is decent. Will improve further.

---
+++ crab_AI.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("train.csv")
+df = df.drop("id", axis=1)
+df = df.drop("Sex", axis=1)
+
+y = df["Age"]
+X = df.drop("Age", axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(20, activation="relu"))
+model.add(tf.keras.layers.Dense(1))
+
+model.compile(loss="MAE", optimizer="adam", metrics=["MAE"])
+
+history = model.fit(X, y, epochs=15, validation_split=0.2)
+
+df = pd.read_csv("test.csv")
+ID = df["id"]
+df = df.drop("id", axis=1)
+df = df.drop("Sex", axis=1)
+
+output = model.predict(df).flatten()
+submission = pd.DataFrame(data={"id": ID, "Age": output})
+submission.to_csv("submission.csv", index=False)
+
+plt.plot(history.history["MAE"], label="loss")
+plt.plot(history.history["val_MAE"], label="val_loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
